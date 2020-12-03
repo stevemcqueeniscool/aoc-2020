8c9c0e952e5db0b78cb1cd3691d3b26e959b17bb
Darren Gawley <dg@gawley.email>
2020-12-03 23:02:58 +0000

    changing to a slimmed down day03

--- cpp/day03.cpp
+++ cpp/day03.cpp
@@ -1,42 +1,18 @@
 #include <fstream>
 #include <iostream>
-#include <set>
 #include <string>
-#include <utility>
 #include <vector>
 
-using point_t = std::pair<size_t, size_t>;
-
-class forest
+int count_trees(const std::vector<std::string>& data, int dx, int dy)
 {
-public:
-    forest(const std::vector<std::string> &data)
-        : width_(data[0].size()), height_(data.size())
-    {
-        for (size_t row = 0; row != height_; ++row) {
-            for (size_t col = 0; col != width_; ++col) {
-                if (data[row][col] == '#') {
-                    trees_.emplace(col, row);
-                }
-            }
-        }
-    }
-
-    bool is_tree(size_t x, size_t y) const { return trees_.count({x % width_, y}); }
-
-    size_t width_;
-    size_t height_;
-    std::set<point_t> trees_;
-};
-
-int count_slope(const forest &f, int dx, int dy)
-{
+    size_t width = data[0].size(), height = data.size();
     size_t x = 0, y = 0;
     int result = 0;
-    while (y < f.height_) {
-        if (f.is_tree(x, y)) {
-            ++result;
-        }
+
+    while (y < height) {
+	if (data[y][x % width] == '#') {
+	    ++result;
+	}
         x += dx;
         y += dy;
     }
@@ -49,15 +25,15 @@ int main()
 {
     std::ifstream input(PATH);
     std::string line;
-    std::vector<std::string> lines;
+    std::vector<std::string> data;
     while (std::getline(input, line)) {
-        lines.push_back(line);
+        data.push_back(line);
     }
 
-    forest f{lines};
-    auto result_a = count_slope(f, 3, 1);
-    auto result_b = count_slope(f, 1, 1) * count_slope(f, 3, 1) * count_slope(f, 5, 1) *
-                    count_slope(f, 7, 1) * count_slope(f, 1, 2);
+    auto result_a = count_trees(data, 3, 1);
+    auto result_b = count_trees(data, 1, 1) * count_trees(data, 3, 1) *
+                    count_trees(data, 5, 1) * count_trees(data, 7, 1) *
+                    count_trees(data, 1, 2);
 
     std::cout << result_a << std::endl;
     std::cout << result_b << std::endl;

9a5d4e9e1da0c33fd9e9d1d34a853f30a5feb775
Darren Gawley <dg@gawley.email>
2020-12-03 12:35:05 +0000

    initial commit with day01 solution

---
+++ cpp/day01.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <set>
+#include <utility>
+
+const std::string PATH = "../data/day1.txt";
+
+using opt_pair_t = std::optional<std::pair<int, int>>;
+opt_pair_t find_pair(int target, const std::set<int> &xs)
+{
+    auto result = opt_pair_t{};
+    for (int x : xs) {
+        const auto y = target - x;
+        if (xs.count(y) != 0) {
+            result = std::make_pair(x, y);
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    std::fstream f(PATH, std::ios_base::in);
+    std::set<int> xs;
+    int x;
+    while (f >> x)
+    {
+	xs.insert(x);
+    }
+
+    const auto part_a = find_pair(2020, xs);
+    std::cout << part_a->first * part_a->second << std::endl;
+
+    for (int z: xs) {
+	if (const auto m = find_pair(2020 - z, xs); m) {
+	    std::cout << z * m->first * m->second << std::endl;
+	    break;
+	}
+    }
+
+    return 0;
+}

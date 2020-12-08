9edebcefbf8a2d232a5b194a792ffe0d8c25359c
Darren Gawley <dg@gawley.email>
2020-12-08 09:22:22 +0000

    adding working version for day08 part b

--- cpp/day08.cpp
+++ cpp/day08.cpp
@@ -9,25 +9,16 @@
 #include <scn/tuple_return.h>
 
 const std::string PATH = "../../data/day08.txt";
-
 using instruction_t = std::pair<std::string, int>;
 
-int main()
+std::pair<bool, int> terminates(const std::vector<instruction_t> &code)
 {
-    std::vector<instruction_t> code;
-    std::ifstream f(PATH);
-    std::string line;
-    while (std::getline(f, line)) {
-        const auto [_, opcode, arg] = scn::scan_tuple<std::string, int>(line, "{} {}");
-        code.emplace_back(opcode, arg);
-    }
-
     size_t pc = 0;
     std::unordered_set<size_t> seen{};
     int acc = 0;
 
     while (true) {
-        if (seen.count(pc) > 0) {
+        if ((pc >= code.size()) || (seen.count(pc) > 0)) {
             break;
         }
 
@@ -42,7 +33,35 @@ int main()
             ++pc;
         }
     }
+    return {pc >= code.size(), acc};
+}
 
-    std::cout << acc << std::endl;
+int main()
+{
+    std::vector<instruction_t> code;
+    std::ifstream f(PATH);
+    std::string line;
+    while (std::getline(f, line)) {
+        const auto [_, opcode, arg] = scn::scan_tuple<std::string, int>(line, "{} {}");
+        code.emplace_back(opcode, arg);
+    }
+
+    const auto [_, result_a] = terminates(code);
+    std::cout << result_a << std::endl;
+
+    int result_b = 0;
+    for (size_t i = 0; i != code.size(); ++i) {
+        if (code[i].first == "jmp") {
+            code[i].first = "nop";
+            const auto [term, acc] = terminates(code);
+            if (term) {
+                result_b = acc;
+                break;
+            }
+            code[i].first = "jmp";
+        }
+    }
+    std::cout << result_b << std::endl;
+
     return 0;
 }

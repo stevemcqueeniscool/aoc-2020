190558ab6393e481b85c580d57e39b1b3f75ad04
Darren Gawley <dg@gawley.email>
2020-12-18 16:47:35 +0000

    making day18 work for part a / b together

--- cpp/day18.cpp
+++ cpp/day18.cpp
@@ -6,69 +6,55 @@
 #include <vector>
 
 const auto PATH = "../../data/day18.txt";
-
 using val_t = std::uint64_t;
 
-char pop(std::vector<char> &tokens)
-{
-    auto r = tokens.back();
-    tokens.pop_back();
-    return r;
-}
+val_t expr(std::vector<char> &tokens, bool prefer_plus);
 
-val_t expr(std::vector<char> &tokens);
-
-val_t value(std::vector<char> &tokens)
+val_t value(std::vector<char> &tokens, bool prefer_plus)
 {
     val_t val;
-    char ch = pop(tokens);
+    auto ch = tokens.back();
+    tokens.pop_back();
     if (ch == '(') {
-        val = expr(tokens);
-        pop(tokens); // ')'
+        val = expr(tokens, prefer_plus);
+        tokens.pop_back(); // ')'
     } else {
         val = ch - '0';
     }
     return val;
 }
 
-val_t expr(std::vector<char> &tokens)
+val_t expr(std::vector<char> &tokens, bool prefer_plus)
 {
-    auto acc = value(tokens);
+    auto acc = value(tokens, prefer_plus);
     while (!tokens.empty() && (tokens.back() == '+' || tokens.back() == '*')) {
-        char op = pop(tokens);
-        auto rhs = value(tokens);
+        auto op = tokens.back();
+        tokens.pop_back();
+        val_t rhs = (op == '*' && prefer_plus) ? expr(tokens, prefer_plus)
+                                               : value(tokens, prefer_plus);
         acc = op == '+' ? (acc + rhs) : (acc * rhs);
     }
     return acc;
-}
-
-std::vector<char> tokenize(const std::string &line)
-{
-    std::vector<char> result;
-    for (const auto ch : line) {
-        if (ch != ' ') {
-            result.push_back(ch);
-        }
-    }
-    std::reverse(result.begin(), result.end());
-    return result;
 }
 
 int main()
 {
     std::ifstream f(PATH);
     std::string line;
-    std::vector<std::string> lines;
+
+    val_t result_a = 0;
+    val_t result_b = 0;
     while (std::getline(f, line)) {
-        lines.push_back(line);
+        std::vector<char> tokens(line.begin(), line.end());
+        tokens.erase(std::remove(tokens.begin(), tokens.end(), ' '), tokens.end());
+        std::reverse(tokens.begin(), tokens.end());
+
+        std::vector<char> tokens_a = tokens;
+        result_a += expr(tokens_a, false);
+        result_b += expr(tokens, true);
     }
 
-    val_t total = 0;
-    for (const auto &l : lines) {
-        auto tokens = tokenize(l);
-        total += expr(tokens);
-    }
-    std::cout << total << std::endl;
+    std::cout << result_a << "\n" << result_b << std::endl;
 
     return 0;
 }

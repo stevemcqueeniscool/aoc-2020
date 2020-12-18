4718b959b5dff3ad35e0905435d0dbc4908d89cc
Darren Gawley <dg@gawley.email>
2020-12-18 17:04:46 +0000

    small tidy-up of day18

--- cpp/day18.cpp
+++ cpp/day18.cpp
@@ -1,35 +1,37 @@
 #include <algorithm>
 #include <cstdint>
+#include <deque>
 #include <fstream>
 #include <iostream>
+#include <iterator>
+#include <queue>
 #include <string>
-#include <vector>
 
 const auto PATH = "../../data/day18.txt";
 using val_t = std::uint64_t;
 
-val_t expr(std::vector<char> &tokens, bool prefer_plus);
+val_t expr(std::queue<char> &tokens, bool prefer_plus);
 
-val_t value(std::vector<char> &tokens, bool prefer_plus)
+val_t value(std::queue<char> &tokens, bool prefer_plus)
 {
     val_t val;
-    auto ch = tokens.back();
-    tokens.pop_back();
+    auto ch = tokens.front();
+    tokens.pop();
     if (ch == '(') {
         val = expr(tokens, prefer_plus);
-        tokens.pop_back(); // ')'
+        tokens.pop(); // ')'
     } else {
         val = ch - '0';
     }
     return val;
 }
 
-val_t expr(std::vector<char> &tokens, bool prefer_plus)
+val_t expr(std::queue<char> &tokens, bool prefer_plus)
 {
     auto acc = value(tokens, prefer_plus);
-    while (!tokens.empty() && (tokens.back() == '+' || tokens.back() == '*')) {
-        auto op = tokens.back();
-        tokens.pop_back();
+    while (!tokens.empty() && (tokens.front() == '+' || tokens.front() == '*')) {
+        auto op = tokens.front();
+        tokens.pop();
         val_t rhs = (op == '*' && prefer_plus) ? expr(tokens, prefer_plus)
                                                : value(tokens, prefer_plus);
         acc = op == '+' ? (acc + rhs) : (acc * rhs);
@@ -42,16 +44,17 @@ int main()
     std::ifstream f(PATH);
     std::string line;
 
-    val_t result_a = 0;
-    val_t result_b = 0;
+    val_t result_a = 0, result_b = 0;
     while (std::getline(f, line)) {
-        std::vector<char> tokens(line.begin(), line.end());
-        tokens.erase(std::remove(tokens.begin(), tokens.end(), ' '), tokens.end());
-        std::reverse(tokens.begin(), tokens.end());
+        std::deque<char> tokens;
+        std::copy_if(line.begin(), line.end(), std::back_inserter(tokens), [](auto ch) {
+            return ch != ' ';
+        });
 
-        std::vector<char> tokens_a = tokens;
+        std::queue<char> tokens_a(tokens);
         result_a += expr(tokens_a, false);
-        result_b += expr(tokens, true);
+        std::queue<char> tokens_b(tokens);
+        result_b += expr(tokens_b, true);
     }
 
     std::cout << result_a << "\n" << result_b << std::endl;

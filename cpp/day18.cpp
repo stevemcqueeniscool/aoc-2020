c30f77eca9747493c7e841fa19f0ace744f1d275
Darren Gawley <dg@gawley.email>
2020-12-18 21:09:34 +0000

    slimming down day18

--- cpp/day18.cpp
+++ cpp/day18.cpp
@@ -1,39 +1,33 @@
 #include <algorithm>
 #include <cstdint>
-#include <deque>
 #include <fstream>
 #include <iostream>
-#include <iterator>
-#include <queue>
 #include <string>
 
 const auto PATH = "../../data/day18.txt";
 using val_t = std::uint64_t;
 
-val_t expr(std::queue<char> &tokens, bool prefer_plus);
+val_t expr(const std::string &line, size_t &idx, bool pt2);
 
-val_t value(std::queue<char> &tokens, bool prefer_plus)
+val_t value(const std::string &line, size_t &idx, bool pt2)
 {
     val_t val;
-    auto ch = tokens.front();
-    tokens.pop();
+    auto ch = line[idx++];
     if (ch == '(') {
-        val = expr(tokens, prefer_plus);
-        tokens.pop(); // ')'
+        val = expr(line, idx, pt2);
+        ++idx; // ')'
     } else {
         val = ch - '0';
     }
     return val;
 }
 
-val_t expr(std::queue<char> &tokens, bool prefer_plus)
+val_t expr(const std::string &line, size_t &idx, bool pt2)
 {
-    auto acc = value(tokens, prefer_plus);
-    while (!tokens.empty() && (tokens.front() == '+' || tokens.front() == '*')) {
-        auto op = tokens.front();
-        tokens.pop();
-        val_t rhs = (op == '*' && prefer_plus) ? expr(tokens, prefer_plus)
-                                               : value(tokens, prefer_plus);
+    auto acc = value(line, idx, pt2);
+    while (idx != line.size() && (line[idx] == '+' || line[idx] == '*')) {
+        const auto op = line[idx++];
+        val_t rhs = (op == '*' && pt2) ? expr(line, idx, pt2) : value(line, idx, pt2);
         acc = op == '+' ? (acc + rhs) : (acc * rhs);
     }
     return acc;
@@ -46,17 +40,13 @@ int main()
 
     val_t result_a = 0, result_b = 0;
     while (std::getline(f, line)) {
-        std::deque<char> tokens;
-        std::copy_if(line.begin(), line.end(), std::back_inserter(tokens), [](auto ch) {
-            return ch != ' ';
-        });
+        line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
 
-        std::queue<char> tokens_a(tokens);
-        result_a += expr(tokens_a, false);
-        std::queue<char> tokens_b(tokens);
-        result_b += expr(tokens_b, true);
+        size_t idx = 0;
+        result_a += expr(line, idx, false);
+        idx = 0;
+        result_b += expr(line, idx, true);
     }
-
     std::cout << result_a << "\n" << result_b << std::endl;
 
     return 0;

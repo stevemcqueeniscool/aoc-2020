56ac0c4b022b3f409c330da02d01bc98d81ec499
Darren Gawley <dg@gawley.email>
2020-12-24 14:21:42 +0000

    tidy-up for day24

--- cpp/day24.cpp
+++ cpp/day24.cpp
@@ -1,102 +1,65 @@
 #include <fstream>
 #include <iostream>
 #include <regex>
-#include <set>
 #include <string>
 #include <utility>
 
 #include "absl/container/flat_hash_set.h"
 
 const std::string PATH = "../../data/day24.txt";
-using path_t = std::vector<std::string>;
-
-struct vec {
-    int x_;
-    int y_;
-
-    template <typename H> friend H AbslHashValue(H h, const vec &v)
-    {
-        return H::combine(std::move(h), v.x_, v.y_);
-    }
-};
-
+using vec = std::pair<int, int>;
 using board_t = absl::flat_hash_set<vec>;
 
-bool operator==(const vec &a, const vec &b)
+vec add_vec(const vec &a, const vec &b)
 {
-    return std::make_pair(a.x_, a.y_) == std::make_pair(b.x_, b.y_);
+    return {a.first + b.first, a.second + b.second};
 }
-
-vec operator+(const vec &a, const vec &b) { return {a.x_ + b.x_, a.y_ + b.y_}; }
 
 auto deltas =
     std::map<std::string, vec>{{"e", {1, 0}},    {"w", {-1, 0}}, {"ne", {0, -1}},
                                {"nw", {-1, -1}}, {"se", {1, 1}}, {"sw", {0, 1}}};
 
-board_t potential_tiles(const board_t &blacks)
+int main()
 {
-    auto result = board_t{};
-    for (const auto &p : blacks) {
-        for (const auto &[_, d] : deltas) {
-            result.insert(p + d);
+    auto f = std::ifstream{PATH};
+    auto dir_regex = std::regex{"sw|se|nw|ne|w|e"};
+    auto line = std::string{};
+    auto today = board_t{};
+    while (std::getline(f, line)) {
+        auto pos = vec{0, 0};
+        auto matches_begin = std::sregex_iterator(line.begin(), line.end(), dir_regex);
+        auto matches_end = std::sregex_iterator();
+        for (auto i = matches_begin; i != matches_end; ++i) {
+            pos = add_vec(pos, deltas[(*i)[0]]);
+        }
+        if (today.count(pos) > 0) {
+            today.erase(pos);
+        } else {
+            today.insert(pos);
         }
     }
-    return result;
-}
 
-size_t simulate(board_t today, int days)
-{
-    for (int day = 0; day != days; ++day) {
+    std::cout << today.size() << std::endl;
+
+    for (int day = 0; day != 100; ++day) {
         auto tomorrow = board_t{};
-        for (const auto &p : potential_tiles(today)) {
-            auto ncount = std::count_if(
-                deltas.begin(), deltas.end(),
-                [&p, &today](const auto &d) { return today.count(p + d.second) > 0; });
-
-            if ((today.count(p) && (ncount == 1 || ncount == 2)) ||
-                (today.count(p) == 0 && ncount == 2)) {
-                tomorrow.insert(p);
+        for (const auto &b : today) {
+            for (const auto &[_, dir] : deltas) {
+                const auto p = add_vec(b, dir);
+                auto ncount = std::count_if(
+                    deltas.begin(), deltas.end(), [&p, &today](const auto &d) {
+                        return today.count(add_vec(p, d.second)) > 0;
+                    });
+                auto black = today.count(p) > 0;
+                if ((black && (ncount == 1 || ncount == 2)) ||
+                    (!black && ncount == 2)) {
+                    tomorrow.insert(p);
+                }
             }
         }
         today = std::move(tomorrow);
     }
-    return today.size();
-}
 
-int main()
-{
-    auto direction_regex = std::regex{"ne|nw|se|sw|e|w"};
-
-    auto paths = std::vector<path_t>{};
-
-    auto f = std::ifstream{PATH};
-    auto line = std::string{};
-    while (std::getline(f, line)) {
-        auto path = path_t{};
-        auto matches_begin =
-            std::sregex_iterator(line.begin(), line.end(), direction_regex);
-        auto matches_end = std::sregex_iterator();
-        for (auto i = matches_begin; i != matches_end; ++i) {
-            path.push_back((*i)[0]);
-        }
-        paths.push_back(path);
-    }
-
-    auto blacks = board_t{};
-    for (const auto &path : paths) {
-        auto pos = vec{0, 0};
-        for (const auto &move : path) {
-            pos = pos + deltas[move];
-        }
-
-        if (blacks.count(pos) > 0) {
-            blacks.erase(pos);
-        } else {
-            blacks.insert(pos);
-        }
-    }
-
-    std::cout << blacks.size() << std::endl;
-    std::cout << simulate(blacks, 100) << std::endl;
+    std::cout << today.size() << std::endl;
     return 0;
 }
